bc5886efb81dcdb329510c569fc6d207c2f3e836
Kingshuk <kinghos@github.com>
2026-02-28 16:29:23 +0000

    changes for maya

---
+++ scenes/base.gd
@@ -0,0 +1,6 @@
+extends Sprite2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
