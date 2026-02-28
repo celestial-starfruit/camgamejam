a7d25fe0e7fefba5c11678c89ace662d33ca5942
Maya Stephens <celestialstarfruit202@gmail.com>
2026-02-28 20:54:30 +0000

    Add animated fire

---
+++ scenes/towers/fire.gd
@@ -0,0 +1,15 @@
+extends Tower
+
+# const PROJECTILE = preload("uid://c7tdsm07l0w3v")
+# @onready var proj_manager: Node2D = $ProjManager
+
+func attack() -> void:
+	print("hi")
+	get_node("CPUParticles2D").emitting = true
+#	var proj: Area2D
+#	var coords = [Vector2(0, 1), Vector2(0, -1), Vector2(-1, 0), Vector2(1, 0)]
+#	for dir in coords:
+#		proj = PROJECTILE.instantiate()
+#		proj.direction = dir
+#		proj_manager.add_child(proj)
+		
