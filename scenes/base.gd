e2fa83cf352f315f25eec8708369d36085ea8b83
Kingshuk <kinghos@github.com>
2026-03-01 01:28:27 +0000

    jingle!

--- scenes/base.gd
+++ scenes/base.gd
@@ -1,5 +1,7 @@
 extends AnimatedSprite2D
 var base_num = 0
+@onready var asp: AudioStreamPlayer = $AudioStreamPlayer
+const BUILDING = preload("uid://dk048nwgtwraa")
 
 signal player_reached
 func _process(delta: float) -> void:
@@ -11,4 +13,7 @@ func _process(delta: float) -> void:
 func _on_area_body_entered(body: Node2D) -> void:
 	if body is Player and self == Globals.target_base:
 		print("player reached target")
+		Music.stop_music()
+		asp.play()
+		Music.play_music(BUILDING)
 		player_reached.emit()
